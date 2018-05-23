6ba93dab572308edf07632710b0b13feb67c00ab
Jerome <ambroise.jerome@gmail.com>
2018-05-23 12:38:26 +0200

    Directives structurelles : *ngIF et *ngFor

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -9,4 +9,17 @@ export class Product {
 }
 
 // Création d'un produit de test
-export const PRODUCT_TEST = new Product('Hamac', 'Pour se reposer après une semaine d\'Angular');
+export const PRODUCT_TEST: Product = new Product('Hamac', 'Pour se reposer après une semaine d\'Angular');
+
+// Création d'un tableau de produits de test
+// Les deux notations suivantes sont équivalentes :
+// - Product[]
+// - Array<Product>
+export const TAB_PRODUCTS: Product[] = [
+    new Product('Hamac', 'Pour se reposer après une semaine d\'Angular'),
+    new Product('Voiture', 'Véhicule à 4 roues avec un moteur et un volant'),
+    new Product('Tongue', 'Chaussure d\'été'),
+    new Product('Vélo', 'Moyen de transport à 2 roues (ou 4)'),
+    new Product('Moto', 'Véhicule à 2 roues (ou 3)'),
+    new Product('Avion', 'Moyen de transport volant (passager ou fret)'),
+];
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Product, PRODUCT_TEST, TAB_PRODUCTS } from '../model/product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
 
-  constructor() { }
+  // Tableau de produits
+  private products: Product[];
+  // Produit sélectionné (null par défaut)
+  public selectedProduct: Product;
 
-  ngOnInit() {
+  constructor() {
+    // Initialisation le tableau de produits
+    this.products = TAB_PRODUCTS;
+    // On automatisera la sélection du produit ASAP
+    // this.selectedProduct = PRODUCT_TEST;
   }
 
 }
